087f5f0fa6047ce54ed1a0e465a137f29ac48de1
Abdelrahman-Hisham <36193968+Abdelrahman-Hisham@users.noreply.github.com>
2019-02-17 21:15:51 +0200

    new file

--- Toggle.py
+++ Toggle.py
@@ -1,16 +1,39 @@
 
-import tkinter as tk
-def toggle():
-    '''
-    use
-    t_btn.config('text')[-1]
-    to get the present state of the toggle button
-    '''
-    if t_btn.config('text')[-1] == 'Pause':
-        t_btn.config(text='Continue')
-    else:
-        t_btn.config(text='Pause')
-root = tk.Tk()
-t_btn = tk.Button(text="Pause", width=12, command=toggle)
-t_btn.pack(pady=5)
-root.mainloop()
+from PyQt5.QtWidgets import (QPushButton , QApplication)
+
+import sys
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        self.col = QColor(0, 0, 0)       
+
+        toggler = QPushButton('Pause', self)
+        toggler.setCheckable(True)
+        toggler.move(10, 10)
+        self.show()
+        
+        
+    def yourfunction(self, pressed):  #Ekteb el functin elly 3ayzha t7sa lw el button pressed
+        
+        source = self.sender()
+        
+     if pressed:
+            #El Event elly hy3ml pause (ex: event2.clear)
+       else:
+            #event.set>>>> 3shan y continue
+                        
+       
+       
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
